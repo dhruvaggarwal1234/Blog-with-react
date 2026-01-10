4ef86ee6fde0f330ec5485f893116af22105846e
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2026-01-10 14:27:14 +0530

    Appwrite Completed

---
+++ Frontend/src/appwrite/auth.js
@@ -0,0 +1,82 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService{
+
+
+client = new Client();
+account;
+
+constructor(){
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+    
+    this.account = new Account(this.client);
+}
+
+
+async createAccount({email , password ,name}){
+
+    try{
+
+        const userAccount = await this.account.create(ID.unique() , email ,password,name);
+
+        if(userAccount){
+            return this.login({email,password});
+        }
+
+        else{
+            return userAccount;
+        }
+
+    }
+    catch(error){
+        console.log("This is Error From CreateAccount :" , error);
+    }
+}
+
+
+async login({email,password}){
+    try{
+        return await this.account.createEmailPasswordSession(email,password);
+
+    }
+    catch(error){
+        console.log("This is Login Error :" , error);
+    }
+}
+
+
+async getCurrentUser(){
+    try{
+       return this.account.get();
+    }
+    catch(error){
+        console.log("This is getCurrentUser Error :" , error)
+    }
+
+     return null;
+}
+
+async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("This is Logout Error :", error);
+        }
+    }
+
+}
+
+
+
+const authService = new AuthService();
+
+export default authService;
+
+
+
+
