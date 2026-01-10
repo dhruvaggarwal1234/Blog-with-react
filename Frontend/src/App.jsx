eca1c4f80d5f40e54e30b266c54ec711af0272c8
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2026-01-10 15:21:33 +0530

    Ui ki Starting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
+
+import authService from "./appwrite/auth"
+import {login,logout} from './store/authSlice'
+import Header from "./components/Header/Header.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 
 
 
 function App() {
+
+   const [loading , SetLoading] = useState(true);
+   const dispatch = useDispatch();
+
+   useEffect(()=>{
+
+    authService.getCurrentUser()
+    .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}));
+        }else{
+          dispatch(logout());
+        }
+    })
+    .finally(() => {SetLoading(false) })
+
+   },[])
+
+   return !loading ?(<div className="min-h-screen flex  flex-wrap content-between bg-gray-400 ">
+
+    <div className="w-full block">
+      <Header />
+      <Footer />
+    </div>
+
+
+   </div> ): null;
   
- const app  = import.meta.env.VITE_APPWRITE_URL;
-  return (
-    <>
- <div>Blog App with this is my url {app} React</div>
-    </>
-  )
+
+
 }
 
 export default App
--- Frontend/src/conf/conf.js
+++ Frontend/src/conf/conf.js
@@ -1,6 +1,6 @@
 
 const conf  = {
-
+    
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
